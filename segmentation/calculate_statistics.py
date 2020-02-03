c453a9953a7bf13d95f267d33ea5b13cb9cfefd2
twagenaar <tessawagenaar@quicknet.nl>
2020-02-03 17:21:50 +0100

    calculate statistics both datasets

--- segmentation/calculate_statistics.py
+++ segmentation/calculate_statistics.py
@@ -20,31 +20,62 @@ statistics["patients"] = os.listdir(root_dir)
 statistics["n_patients"] = len(statistics["patients"])
 
 
-MIN_HV = -100
-MAX_HV = 300
+MIN_HV = -500
+MAX_HV = 500
 
 print("calculate image data")
 for patient in statistics["patients"]:
-    print(patient)
-    images = glob.glob(str(root_dir / patient / "*.nii"))
-    for img in images:
-        if ("cervix_uterus_" not in img) and ("bladder_" not in img):
-            if img not in statistics:
-                statistics[img] = {}
-            image, metadata = read_image(img)
-            hist, bins = np.histogram(image, bins=100, range=(MIN_HV, MAX_HV))
-            statistics[img]["image_hist"] = (hist, bins)
-            statistics[img]["min"] = image.min()
-            statistics[img]["max"] = image.max()
-            statistics[img]["mean"] = image.mean()
-            clipped_image = np.clip(image, MIN_HV, MAX_HV)
-            normalized_image = (clipped_image - MIN_HV) / (MAX_HV - MIN_HV)
-            statistics[img]["norm_mean"] = (normalized_image).mean()
-            hist_norm, bins_norm = np.histogram(
-                normalized_image, bins=100, range=(0, 1))
-            statistics[img]["norm_image_hist"] = (hist_norm, bins_norm)
-            print(hist)
-print(bins, bins_norm)
+    # images = glob.glob(str(root_dir / patient / "full/*.nrrd"))
+    # for img in images:
+    #     if ("cervix_uterus_" not in img) and ("bladder_" not in img):
+    #         if img not in statistics:
+    #             statistics[img] = {}
+    #         image, metadata = read_image(img)
+    #         hist, bins = np.histogram(image, bins=100, range=(MIN_HV, MAX_HV))
+    #         statistics[img]["image_hist"] = (hist, bins)
+    #         statistics[img]["min"] = image.min()
+    #         statistics[img]["max"] = image.max()
+    #         statistics[img]["mean"] = image.mean()
+    #         clipped_image = np.clip(image, MIN_HV, MAX_HV)
+    #         normalized_image = (clipped_image - MIN_HV) / (MAX_HV - MIN_HV)
+    #         statistics[img]["norm_mean"] = (normalized_image).mean()
+    #         hist_norm, bins_norm = np.histogram(
+    #             normalized_image, bins=100, range=(0, 1))
+    #         statistics[img]["norm_image_hist"] = (hist_norm, bins_norm)
+    #         print(hist)
+    img = str(root_dir / patient / "CT1.nii")
+    image = read_image(img, no_meta=True) - 1000
+    if img not in statistics:
+        statistics[img] = {}
+    hist, bins = np.histogram(image, bins=100, range=(MIN_HV, MAX_HV))
+    statistics[img]["image_hist"] = (hist, bins)
+    statistics[img]["min"] = image.min()
+    statistics[img]["max"] = image.max()
+    statistics[img]["mean"] = image.mean()
+    clipped_image = np.clip(image, MIN_HV, MAX_HV)
+    normalized_image = (clipped_image - MIN_HV) / (MAX_HV - MIN_HV)
+    statistics[img]["norm_mean"] = (normalized_image).mean()
+    hist_norm, bins_norm = np.histogram(
+        normalized_image, bins=100, range=(0, 1))
+    statistics[img]["norm_image_hist"] = (hist_norm, bins_norm)
+    print(hist)
+
+    # img = str(root_dir / patient / "empty/CT.nrrd")
+    # image = read_image(img, no_meta=True)
+    # if img not in statistics:
+    #     statistics[img] = {}
+    # hist, bins = np.histogram(image, bins=100, range=(MIN_HV, MAX_HV))
+    # statistics[img]["image_hist"] = (hist, bins)
+    # statistics[img]["min"] = image.min()
+    # statistics[img]["max"] = image.max()
+    # statistics[img]["mean"] = image.mean()
+    # clipped_image = np.clip(image, MIN_HV, MAX_HV)
+    # normalized_image = (clipped_image - MIN_HV) / (MAX_HV - MIN_HV)
+    # statistics[img]["norm_mean"] = (normalized_image).mean()
+    # hist_norm, bins_norm = np.histogram(
+    #     normalized_image, bins=100, range=(0, 1))
+    # statistics[img]["norm_image_hist"] = (hist_norm, bins_norm)
+# print(bins, bins_norm)
 
 pickle.dump(statistics, open("image_statistics.p", 'wb'))
 
--- segmentation/statistics.py
+++ segmentation/statistics.py
@@ -4,7 +4,6 @@ import numpy as np
 statistics = pickle.load(open("image_statistics.p", 'rb'))
 
 files = list(statistics.keys())[2:]
-print(statistics[files[0]].keys())
 
 means = []
 norm_means = []
@@ -16,4 +15,34 @@ for f in files:
     mins.append(statistics[f]["min"])
     maxs.append(statistics[f]["max"])
 
-print(np.mean(means), np.mean(norm_means), np.min(mins), np.max(maxs))
+print()
+print("CBCT dataset CTs:")
+print("mean:", np.mean(means))
+print("normalized mean:", np.mean(norm_means))
+print("min:", np.min(mins))
+print("max:", np.max(maxs))
+
+
+
+statistics = pickle.load(open("image_statistics_extra.p", 'rb'))
+
+files = list(statistics.keys())[2:]
+
+means = []
+norm_means = []
+mins = []
+maxs = []
+for f in files:
+    means.append(statistics[f]["mean"])
+    norm_means.append(statistics[f]["norm_mean"])
+    mins.append(statistics[f]["min"])
+    maxs.append(statistics[f]["max"])
+
+print()
+print("extra dataset CTs:")
+print("mean:", np.mean(means))
+print("normalized mean:", np.mean(norm_means))
+print("min:", np.min(mins))
+print("max:", np.max(maxs))
+
+
