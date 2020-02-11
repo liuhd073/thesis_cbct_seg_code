697083baae716c1d482301942da58d21e9143f43
twagenaar <tessawagenaar@quicknet.nl>
2020-02-11 12:51:14 +0100

    Added standard shape

--- segmentation/calculate_statistics.py
+++ segmentation/calculate_statistics.py
@@ -18,10 +18,11 @@ statistics = {}
 # List all the patients
 statistics["patients"] = os.listdir(root_dir)
 statistics["n_patients"] = len(statistics["patients"])
+statistics["spacing"] = []
 
 
-MIN_HV = -500
-MAX_HV = 500
+MIN_HV = -100
+MAX_HV = 300
 
 print("calculate image data")
 for patient in statistics["patients"]:
@@ -43,25 +44,28 @@ for patient in statistics["patients"]:
     #             normalized_image, bins=100, range=(0, 1))
     #         statistics[img]["norm_image_hist"] = (hist_norm, bins_norm)
     #         print(hist)
-    img = str(root_dir / patient / "CT1.nii")
-    image = read_image(img, no_meta=True) - 1000
-    if img not in statistics:
-        statistics[img] = {}
-    hist, bins = np.histogram(image, bins=100, range=(MIN_HV, MAX_HV))
-    statistics[img]["image_hist"] = (hist, bins)
-    statistics[img]["min"] = image.min()
-    statistics[img]["max"] = image.max()
-    statistics[img]["mean"] = image.mean()
-    clipped_image = np.clip(image, MIN_HV, MAX_HV)
-    normalized_image = (clipped_image - MIN_HV) / (MAX_HV - MIN_HV)
-    statistics[img]["norm_mean"] = (normalized_image).mean()
-    hist_norm, bins_norm = np.histogram(
-        normalized_image, bins=100, range=(0, 1))
-    statistics[img]["norm_image_hist"] = (hist_norm, bins_norm)
-    print(hist)
+    img = root_dir / patient / "CT1.nii"
+    if img.exists():
+        image, meta = read_image(img)
+        # image -= 1000
+        if img not in statistics:
+            statistics[img] = {}
+        hist, bins = np.histogram(image, bins=100, range=(MIN_HV, MAX_HV))
+        statistics[img]["image_hist"] = (hist, bins)
+        statistics[img]["min"] = image.min()
+        statistics[img]["max"] = image.max()
+        statistics[img]["mean"] = image.mean()
+        clipped_image = np.clip(image, MIN_HV, MAX_HV)
+        normalized_image = (clipped_image - MIN_HV) / (MAX_HV - MIN_HV)
+        statistics[img]["norm_mean"] = (normalized_image).mean()
+        hist_norm, bins_norm = np.histogram(
+            normalized_image, bins=100, range=(0, 1))
+        statistics[img]["norm_image_hist"] = (hist_norm, bins_norm)
+        statistics["spacing"].append(meta["spacing"])
+        print(hist)
 
     # img = str(root_dir / patient / "empty/CT.nrrd")
-    # image = read_image(img, no_meta=True)
+    # image, meta = read_image(img)
     # if img not in statistics:
     #     statistics[img] = {}
     # hist, bins = np.histogram(image, bins=100, range=(MIN_HV, MAX_HV))
@@ -75,6 +79,7 @@ for patient in statistics["patients"]:
     # hist_norm, bins_norm = np.histogram(
     #     normalized_image, bins=100, range=(0, 1))
     # statistics[img]["norm_image_hist"] = (hist_norm, bins_norm)
+    # statistics["spacing"].append(meta["spacing"])
 # print(bins, bins_norm)
 
 pickle.dump(statistics, open("image_statistics.p", 'wb'))
