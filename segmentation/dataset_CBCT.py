32fe2be869a5fc49541a0c424b3b38b9f9037eee
twagenaar <tessawagenaar@quicknet.nl>
2020-07-14 17:06:57 +0200

    change to resampled CBCTs

--- segmentation/dataset_CBCT.py
+++ segmentation/dataset_CBCT.py
@@ -13,13 +13,13 @@ import numpy as np
 
 
 class CBCTDataset(Dataset):
-    def __init__(self, files, n_slices=21, transform=None, cachedir="/cache", clipped=False):
+    def __init__(self, files, n_slices=21, transform=None, cachedir="/cache"):
         self.n_slices = n_slices
-        self.clipped = clipped
-        self.image_shapes = {tup[0]: (tup[1][1], tup[1][0], tup[1][2]) for tup in files}
+        self.image_shapes = {tup[0]: (tup[1][0], tup[1][1], tup[1][2]) for tup in files}
         self.data = {tup[0]: (tup[2], tup[3]) for tup in files}
         self.patients = list(self.image_shapes.keys())
         self.transform = transform
+        print(self.image_shapes[self.patients[0]], self.image_shapes[self.patients[0]][-3])
         self.cumulative_imshapes = [0] + list(
             np.cumsum([self.image_shapes[patient][-3] for patient in self.patients])
         )
@@ -33,13 +33,15 @@ class CBCTDataset(Dataset):
         if len(segmentations) == 2:
             seg_bladder = read_image(segmentations[0], no_meta=True)
             seg_cervix_uterus = read_image(segmentations[1], no_meta=True)
-        
-        start = int((seg_bladder.shape[1] - 512) / 2)
+
+        start = 0
+        seg_bladder = crop_to_bbox(seg_bladder, (0, start, start, seg_bladder.shape[0], 512, 512))
+        seg_cervix_uterus = crop_to_bbox(seg_cervix_uterus, (0, start, start, seg_cervix_uterus.shape[0], 512, 512))
         all_segs = seg_bladder + seg_cervix_uterus
         other = all_segs < 1
         segs = [seg_bladder, seg_cervix_uterus, other]
         segmentation = np.stack(segs).astype(int)
-        return segmentation.swapaxes(1,2)
+        return segmentation
 
     def _load_image(self, patient):
         cache_fn = self.cachedir / f"{patient}_CT1"
@@ -51,14 +53,6 @@ class CBCTDataset(Dataset):
             image_path, segmentation_paths = self.data[patient]
             image = read_image(image_path, no_meta=True)
             segmentation = self._get_segmentation(segmentation_paths)
-            if not self.clipped:
-                keep = [i % 3 ==0 for i in range(256)]
-                image = image[:,keep,:]
-                segmentation = segmentation[:,keep,:,:]
-            image = image.swapaxes(0,1)
-            assert (
-                image.shape == segmentation.shape[1:]
-            ), "image and segmentation should be of same shape in dataset!"
             if len(image.shape) == 3:
                 # add "channels" dimension if it is not present
                 image = np.expand_dims(image, axis=0)
@@ -82,17 +76,20 @@ class CBCTDataset(Dataset):
             self.image, self.segmentation = self._load_image(patient)
             self.current_patient = patient
         
-        start = int((self.image_shapes[patient][1] - 512) / 2)
+        start = 0
         middle_slice = self.n_slices // 2
 
         im_slice = crop_to_bbox(self.image, (0, slice_idx - middle_slice, start, start, 1, self.n_slices, 512, 512))
-        seg_slice = crop_to_bbox(self.segmentation, (0, slice_idx, start, start, 3, 1, 512, 512))
+        seg_slice = crop_to_bbox(self.segmentation, (0, slice_idx, 0, 0, 3, 1, 512, 512))
         
         sample = {"image": im_slice.squeeze(0), "target": seg_slice.squeeze(1)}
         sample = self.transform(sample)
         im_slice = np.expand_dims(sample["image"],0)
         seg_slice = np.expand_dims(sample["target"],1)
         
+        # assert (
+        #     image.shape == segmentation.shape[1:]
+        # ), "image and segmentation should be of same shape in dataset!"
         assert (
             0 not in seg_slice.shape
         ), f"Segmentation slice has dimension of size 0: {seg_slice.shape}"
